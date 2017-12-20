ec44deba5166297abe5c907b5cb249ad3c7109f0
Sean Wu <panda@ms5.url.com.tw>
2017-12-20 14:39:01 +0800

    Add files via upload

--- plot_decision_regions.py
+++ plot_decision_regions.py
@@ -3,19 +3,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_decision_regions(X, y, classifier, resolution=0.01):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.01):
     markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])    
+    colors = ('red', 'blue', 'lightgreen', 'black', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # 計算平面所有的點座標
     x1_min, x1_max = X[:,0].min() - 1,X[:,0].max() + 1
     x2_min, x2_max = X[:,1].min() - 1,X[:,1].max() + 1
+    
     # xx1 圖面上所有x點 ==> x1 * x2 矩陣中,只取x1所構成的矩陣
     # xx2 圖面上所有y點 ==> x1 * x2 矩陣中,只取x2所構成的矩陣
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
+    
     # np.array([xx1.ravel(),xx2.ravel()]).T 將xx1,xx2的資料合併變成一個個的座標點(xx1,xx2)
     # classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T) 將一個個的座標點轉換為 1 or  -1(感知器的輸出)
-    z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    
     # 將感知器的輸出轉成 x1 * x2 矩陣, 因此每一個座標都可以mapping到輸出值
     z = z.reshape(xx1.shape)
     
@@ -25,5 +30,12 @@ def plot_decision_regions(X, y, classifier, resolution=0.01):
 
     # y 即是分類結果，此範例有[1, -1]
     for idx, cl in enumerate(np.unique(y)):
+        
         # 將分類結為1的點畫一顏色，-1的對畫另一個顏色
         plt.scatter(x=X[y == cl, 0],y=X[y == cl, 1],alpha=0.4, c=cmap(idx), marker=markers[idx], label=cl)
+
+    # 描繪測試資料
+    
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='',edgecolors='r', alpha=1.0, linewidths=1, marker='o', s=55, label = 'test set')
---
+++ skl_perceptron.py
@@ -0,0 +1,60 @@
+from sklearn import datasets
+import numpy as np
+import plot_decision_regions as pdr
+import matplotlib.pyplot as plt
+
+# 將資料分為訓練集與測試集函數
+from sklearn.cross_validation import train_test_split
+
+# 將資料正規化函數
+from sklearn.preprocessing import StandardScaler
+
+# 感知器函數
+from sklearn.linear_model import Perceptron
+
+# 效能指標模組
+# accuracy_score 算正確率
+from sklearn.metrics import accuracy_score
+
+# 使用scikit-learn中內建的訓練資料
+# 鳶尾花數據
+iris = datasets.load_iris() 
+X = iris.data[:, [2,3]]
+y = iris.target
+
+# 將資料分為訓練集(70%)與測試集(30%)
+# random_state 0 表示不執行seed(), 即每次執行順序會不同
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# 建立正規化物件
+sc = StandardScaler()
+
+# 利用現有資料計算正規化所需的平均值與標準差
+# 不執行此指令則之後的動作都無法操作
+sc.fit(X_train) 
+
+# 查平均值與標準差，回傳陣列[平均值，標準差]
+sc.mean_
+
+# 利用sc.fit算出來的平均值與標準差來正規化訓練集與測試集
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# 模型訓練
+ppn = Perceptron(max_iter=40, eta0=0.01, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+# 預測
+y_pred = ppn.predict(X_test_std)
+print(accuracy_score(y_test, y_pred))
+
+
+# 繪圖
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+pdr.plot_decision_regions(X=X_combined_std, y=y_combined,
+                          classifier=ppn, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc="upper left")
+plt.show()
