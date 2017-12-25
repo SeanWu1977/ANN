23128b6c5e3e3f8bda0c907b3400a1e3b23d6296
Sean Wu <panda@ms5.url.com.tw>
2017-12-25 17:20:54 +0800

    Add files via upload

--- skl_decisiontree.py
+++ skl_decisiontree.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import numpy as np
 import plot_decision_regions as pdr
 import matplotlib.pyplot as plt
+import graphviz 
 
 # 將資料分為訓練集與測試集函數
 from sklearn.model_selection import train_test_split
@@ -9,10 +10,11 @@ from sklearn.model_selection import train_test_split
 # 將資料正規化函數
 from sklearn.preprocessing import StandardScaler
 
-# 感知器函數
+# 決策樹
 from sklearn.tree import DecisionTreeClassifier as dt
 from sklearn.tree import export_graphviz as eg
-
+from sklearn.tree import DecisionTreeRegressor as dtr
+from sklearn.ensemble import RandomForestClassifier as rfc
 
 # 效能指標模組
 # accuracy_score 算正確率
@@ -42,9 +44,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 ##X_train_std = sc.transform(X_train)
 ##X_test_std = sc.transform(X_test)
 
-# 模型訓練
+# 模型訓練{'entropy' or 'gini'(default)}
 tree = dt(criterion='entropy', max_depth=3, random_state=0)
 tree.fit(X_train, y_train)
+
 
 # 預測
 y_pred = tree.predict(X_test)
@@ -54,6 +57,7 @@ print("預測準確率 : {:.2%}".format(accuracy_score(y_test, y_pred)))
 # 繪圖
 X_combined = np.vstack((X_train, X_test))
 y_combined = np.hstack((y_train, y_test))
+plt.figure('Decision Tree')
 pdr.plot_decision_regions(X=X_combined, y=y_combined,
                           classifier=tree, test_idx=range(105,150))
 plt.xlabel('petal length [cm]')
@@ -61,6 +65,44 @@ plt.ylabel('petal width [cm]')
 plt.legend(loc="upper left")
 plt.show()
 
-eg(tree, out_file='tree.dot',feature_names=['petal length','petal width']
-)
+# 將決策數視覺化
+dot_data = eg(tree, out_file='tree.dot', filled=True, rounded=True,  
+                         special_characters=True)
+graph = graphviz.Source(dot_data)
+
+
+##eg(tree, out_file='tree.dot',feature_names=['petal length','petal width']
+##)
+
 # cmd下執行dot -Tpng tree.dot -o tree.png
+
+# n_estimators = 隨機樹數量  ，  n_jobs cpu使用量
+forest = rfc(criterion='entropy', n_jobs=2, random_state=1, n_estimators=10)
+forest.fit(X_train, y_train)
+y_pred = tree.predict(X_test)
+plt.figure('Random Forest')
+print("預測準確率 : {:.2%}".format(accuracy_score(y_test, y_pred)))
+pdr.plot_decision_regions(X=X_combined, y=y_combined,
+                          classifier=forest, test_idx=range(105,150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc="upper left")
+
+plt.show()
+
+
+
+clf = dtr()
+clf.fit(X_train, y_train)
+y_pred = tree.predict(X_test)
+print("預測準確率 : {:.2%}".format(accuracy_score(y_test, y_pred)))
+plt.figure('Decision Tree Regression')
+pdr.plot_decision_regions(X=X_combined, y=y_combined,
+                          classifier=clf, test_idx=range(105,150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc="upper left")
+
+plt.show()
