5485812b1aaa575633232fefcdb88cdc56c880ca
Sean Wu <panda@ms5.url.com.tw>
2020-01-08 20:07:58 +0800

    Update transfer_learning.py

--- 學習筆記/周凡剛/transfer_learning.py
+++ 學習筆記/周凡剛/transfer_learning.py
@@ -16,3 +16,34 @@ vgg.output # tensor
 # Series vs numpy.array
 # Series 只有一維 [[1,1,1],[1,1,1]] (2,)
 # Numpy array 可有多維[[1,1,1],[1,1,1]] (2,3)
+
+
+from keras.preprocessing.image import load_img # 依路徑讀檔
+
+from keras.applications.vgg16 import preprocess_input 
+# 用別人的model 記得要用他的preprocess_input
+# 他會幫忙把輸入轉為該模型的輸入大小
+imagenet_utils.preprocess_input(x, mode='tf', **kwargs)
+
+# 三種 mode
+if mode == 'tf':
+        x /= 127.5
+        x -= 1.
+        return x
+
+    if mode == 'torch':
+        x /= 255.
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+    else:
+        if data_format == 'channels_first':
+            # 'RGB'->'BGR'
+            if x.ndim == 3:
+                x = x[::-1, ...]
+            else:
+                x = x[:, ::-1, ...]
+        else:
+            # 'RGB'->'BGR'
+            x = x[..., ::-1]
+        mean = [103.939, 116.779, 123.68]
+        std = None
